09755820a578a7243b8f67d60bc70a2b8a2599e9
moisesdurandelrazo <moises6737@gmail.com>
2023-02-23 11:08:57 -0600

    Start .net

---
+++ src/app/components/targeta-credito/targeta-credito.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-targeta-credito',
+  templateUrl: './targeta-credito.component.html',
+  styleUrls: ['./targeta-credito.component.css']
+})
+export class TargetaCreditoComponent {
+
+}
